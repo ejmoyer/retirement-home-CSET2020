de60087779535f33a71e77b2e556815babf7b4d9
Eric Moyer <ejmoyer3@comcast.net>
2020-11-10 11:17:23 -0500

    Initialized php for role page.

---
+++ rolePage.php
@@ -0,0 +1,10 @@
+<?php
+// link to database
+
+// query the database for all current roles
+
+// create two textboxes that you can type into (new role and access level)
+// and a submit button
+echo <<<EOT
+EOT
+?>
