cc31fa6ccdaa1f91aeda4d384cf03161c2682242
Eric Moyer <ejmoyer3@comcast.net>
2020-11-10 11:20:26 -0500

    Added input textboxes and submit button for new roles.

--- rolePage.php
+++ rolePage.php
@@ -6,5 +6,14 @@
 // create two textboxes that you can type into (new role and access level)
 // and a submit button
 echo <<<EOT
+query goes here
+
+<label for="newRole">New Role</label>
+<input type="text" name="newRole">
+
+<label for="access">Access Level</label>
+<input type="text" name="access">
+
+<input type="submit">
 EOT
 ?>
