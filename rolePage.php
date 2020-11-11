2406ee4703a01ac5ddd21ab66417cdc8f4d484d5
Eric Moyer <ejmoyer3@comcast.net>
2020-11-11 11:00:42 -0500

    Linked up our current DB and made the page query for all the roles in the database.

--- rolePage.php
+++ rolePage.php
@@ -1,13 +1,45 @@
 <?php
-// link to database
-
+/* link to database */
+$link = mysqli_connect("localhost", "root", "", "retirement");
 // query the database for all current roles
+$query = "SELECT role, accessLevel FROM roles";
+// prepare the statement
+if ($stmt = mysqli_prepare($link, $query)) {
+  // execute statement
+  mysqli_stmt_execute($stmt);
 
-// create two textboxes that you can type into (new role and access level)
-// and a submit button
+  // bind result variable
+  mysqli_stmt_bind_result($stmt, $role, $accessLevel);
+  echo <<<EOT
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">Role</th>
+        <th scope="col">Access Level</th>
+      </tr>
+    </thead>
+    <tbody>
+  EOT;
+  // fetch values
+  while (mysqli_stmt_fetch($stmt)) {
+      printf (<<<EOT
+      <tr>
+        <td>%s</td>
+        <td>%s</td>
+      </tr>
+      EOT, $role, $accessLevel);
+}
 echo <<<EOT
-query goes here
-
+  </tbody>
+</table>
+EOT;
+  // close the statement
+  mysqli_stmt_close($stmt);
+}
+// create two textboxes that you can type into (new role and access level)
+// and a submit button */
+echo <<<EOT
+<form action="rolePage.php" method="post">
 <label for="newRole">New Role</label>
 <input type="text" name="newRole">
 
@@ -15,5 +47,7 @@ query goes here
 <input type="text" name="access">
 
 <input type="submit">
-EOT
+</form>
+EOT;
+mysqli_close($link);
 ?>
