ae02e2e11e22a968c8c5a2a5773b8d353418aabf
Eric Moyer <ejmoyer3@comcast.net>
2020-11-16 10:40:14 -0500

    Converted role page SQL code to object oriented to fit the SQL style we are going with and added placeholder access level check.

--- employee/rolePage.php
+++ employee/rolePage.php
@@ -1,41 +1,47 @@
 <?php
-/* link to database */
-$link = mysqli_connect("localhost", "root", "", "retirement");
+session_start();
+// If they aren't an admin, stop the script.
+if ($_SESSION['access'] > 1) {
+  echo "<p>You shouldn't be here.</p>";
+  exit();
+}
+$mysqli = new mysqli('localhost', 'root', '', 'retirement');
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 /* Start of Insert new Role */
 if (isset($_POST['newRole']) and isset($_POST['access'])) {
   $newRole = $_POST["newRole"];
   $access = $_POST["access"];
 
-  if ($stmt = mysqli_prepare($link, "INSERT INTO roles (role, accessLevel) VALUES (?, ?)")) {
-    mysqli_stmt_bind_param($stmt, "ss", $newRole, $access);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-  }
+  $stmt = $mysqli->prepare("INSERT INTO roles (role, accessLevel) VALUES (?, ?)");
+  $stmt->bind_param("ss", $newRole, $access);
+  $stmt->execute();
+  $stmt->close();
 }
 /* End of Insert New Role */
 
 /* Start of Select All Roles */
 // query the database for all current roles
-$query = "SELECT role, accessLevel FROM roles";
 // prepare the statement
-if ($stmt = mysqli_prepare($link, $query)) {
-  // execute statement
-  mysqli_stmt_execute($stmt);
-
-  // bind result variable
-  mysqli_stmt_bind_result($stmt, $role, $accessLevel);
-  echo <<<EOT
-  <table>
-    <thead>
-      <tr>
-        <th scope="col">Role</th>
-        <th scope="col">Access Level</th>
-      </tr>
-    </thead>
-    <tbody>
-  EOT;
-  // fetch values
-  while (mysqli_stmt_fetch($stmt)) {
+$stmt = $mysqli->prepare("SELECT role, accessLevel FROM roles");
+$stmt->execute();
+$stmt->bind_result($role, $accessLevel);
+echo <<<EOT
+<table>
+  <thead>
+    <tr>
+      <th scope="col">Role</th>
+      <th scope="col">Access Level</th>
+    </tr>
+  </thead>
+  <tbody>
+EOT;
+// fetch values
+  while ($stmt->fetch()) {
       printf (<<<EOT
       <tr>
         <td>%s</td>
@@ -47,9 +53,8 @@ echo <<<EOT
   </tbody>
 </table>
 EOT;
-  // close the statement
-  mysqli_stmt_close($stmt);
-}
+// close the statement
+$stmt->close();
 /* End of Select All Roles */
 
 // create two textboxes that you can type into (new role and access level)
@@ -65,5 +70,5 @@ echo <<<EOT
 <input type="submit">
 </form>
 EOT;
-mysqli_close($link);
+$mysqli->close();
 ?>
