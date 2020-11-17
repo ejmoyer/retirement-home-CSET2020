7ea9ae85b13741950d2eef22682cdbb7f0d6f654
Eric Moyer <ejmoyer3@comcast.net>
2020-11-17 09:53:00 -0500

    Changed role select values to numbers so they are easier to put into SQL, also changed JS to detect patient the same way.

--- authentication/registerPatientFamilyInfo.js
+++ authentication/registerPatientFamilyInfo.js
@@ -6,7 +6,7 @@ select.addEventListener('change', setRole); // when the select is changed
 function setRole() { // function for addEventListener
   const choice = select.value; // get the choice made by select
 
-  if (choice === 'patient') { // If that choice is "patient" show extra data needed for patient
+  if (choice == 5) { // If that choice is 5 to show extra data needed for patient
     databox.innerHTML += '<label for="family_code">Family Code</label> <input type="text" name="family_code">';
     databox.innerHTML += '<label for="emergency_contact">Emergency Contact</label> <input type="text" name="emergency_contact">';
     databox.innerHTML += '<label for="relation_to_contact">Relation to Emergency Contact</label> <input type="text" name="relation_to_contact">';
