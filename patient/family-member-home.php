0bec40285d53af318f9f38a4db1a8d36225ccef5
Eric Moyer <ejmoyer3@comcast.net>
2020-12-07 11:01:45 -0500

    Initialized the table after a family member posts the patientId, date, and family code.

--- patient/family-member-home.php
+++ patient/family-member-home.php
@@ -21,4 +21,37 @@ echo <<<EOT
 <input type=submit value=Logout>
 </form>
 EOT;
+
+echo <<<EOT
+<form action="family-member-home.php" method="post">
+<label for="rosterDate">Date</label>
+<input type="text" name="rosterDate">
+
+<label for="familyCode">Family Code</label>
+<input type="text" name="familyCode">
+
+<label for="patientId">Patient ID</label>
+<input type="text" name="patientId">
+EOT;
+
+if (isset($_POST['rosterDate']) && isset($_POST['familyCode']) && isset($_POST['patientId'])) {
+  echo <<<EOT
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">Doctor's Name</th>
+        <th scope="col">Doctor's Appointment</th>
+        <th scope="col">Caregiver Name</th>
+        <th scope="col">Morning Medicine</th>
+        <th scope="col">Afternoon Medicine</th>
+        <th scope="col">Night Medicine</th>
+        <th scope="col">Breakfast</th>
+        <th scope="col">Lunch</th>
+        <th scope="col">Dinner</th>
+      </tr>
+    </thead>
+    <tbody>
+    EOT;
+}
+$mysqli->close();
 ?>
