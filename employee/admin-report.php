3ee26ed6d5487a131ab2fa0d0d1626e64fd2fcd1
Eric Moyer <ejmoyer3@comcast.net>
2020-12-08 10:51:53 -0500

    Using the patientId list, this page will fill out each of the fields using that and the date that was filled out. The page fills out all fields, but must be checked for people who don't have something missing.

--- employee/admin-report.php
+++ employee/admin-report.php
@@ -40,17 +40,122 @@ if (isset($_POST['checkboxDate'])) {
       $patientsArray[] = $patient;
     }
     $stmt->close();
+
+    echo <<<EOT
+    <h2>Missed Patient Activity</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Patient's Name</th>
+          <th scope="col">Doctor's Name</th>
+          <th scope="col">Doctor's Appointment</th>
+          <th scope="col">Caregiver's Name</th>
+          <th scope="col">Morning Medicine</th>
+          <th scope="col">Afternoon Medicine</th>
+          <th scope="col">Night Medicine</th>
+          <th scope="col">Breakfast</th>
+          <th scope="col">Lunch</th>
+          <th scope="col">Dinner</th>
+        </tr>
+      </thead>
+      <tbody>
+    EOT;
     // for each patientId, fill out a row
     foreach ($patientsArray as $patient) {
-      echo <<<EOT
-      <h2>Missed Patient Activity</h2>
+      if ($stmt = $mysqli->prepare("SELECT firstName, lastName FROM patients JOIN users ON users.id = patients.userId WHERE patientId = ?")) {
+        $stmt->bind_param("i", $patient);
+        $stmt->execute();
+        $stmt->bind_result($patFirstName, $patLastName);
+        while ($stmt->fetch()) {
+          printf (<<<EOT
+          <tr scope="row">
+          <td>%s %s</td>
+          EOT, $patFirstName, $patLastName);
+        }
+        $stmt->close();
 
-      <table>
-        <thead>
-        
-      EOT;
+      if ($stmt = $mysqli->prepare("SELECT firstName, lastName FROM users JOIN employees ON employees.userId = users.id JOIN rosters ON employees.employeeId = rosters.doctorId WHERE rosters.rosterDate = ?")) {
+        $stmt->bind_param("s", $_POST['checkboxDate']);
+        $stmt->execute();
+        $stmt->bind_result($docFirstName, $docLastName);
+        while ($stmt->fetch()) {
+          printf (<<<EOT
+            <td>%s %s</td>
+            EOT, $docFirstName, $docLastName);
+        }
+        $stmt->close();
+        // check if an appointment exists first
+        if ($stmt = $mysqli->prepare("SELECT * FROM appointments JOIN patients ON patients.patientId = appointments.patientId JOIN users ON users.id = patients.userId WHERE appDate = ? and patients.patientId = ?;")) {
+          $stmt->bind_param("si", $_POST['checkboxDate'], $patient);
+          $stmt->execute();
+
+          $stmt->store_result();
+          // if there isn't an appointment, the checkbox will be blank. Otherwise it will be checked.
+          if ($stmt->num_rows() == 0) {
+            echo ("<td><input type=checkbox disabled></td>");
+          } else {
+            echo ("<td><input type=checkbox checked disabled></td>");
+          }
+          $stmt->close();
+          // query to get the rest of the table (caregiver and all the checkboxes)
+          if ($stmt = $mysqli->prepare("SELECT firstName, lastName, morningMed, afternoonMed, nightMed, breakfast, lunch, dinner FROM users JOIN employees ON employees.userId = users.id JOIN checkboxes ON employees.employeeId = checkboxes.caregiverId WHERE checkboxDate = ? and checkboxes.patientId = ?;")) {
+            $stmt->bind_param("si", $_POST['checkboxDate'], $patient);
+            $stmt->execute();
+            $stmt->bind_result($careFirstName, $careLastName, $morningMed, $afternoonMed, $nightMed, $breakfast, $lunch, $dinner);
+            while ($stmt->fetch()) {
+              printf (<<<EOT
+              <td>%s %s</td>
+              EOT, $careFirstName, $careLastName);
+
+              if ($morningMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($afternoonMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($nightMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($breakfast == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($lunch == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($dinner == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+            }
+            $stmt->close();
+            echo "</tr>";
+          }
+        }
+      }
     }
   }
+  echo <<<EOT
+  </tbody>
+  </table>
+  EOT;
+}
 }
 
 // NOTE: Move all this onto another branch. Use the first query to get every patientId on a given day and put them in an array
