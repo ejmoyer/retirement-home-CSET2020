0214ec99596bf11fb784d309cae29a8ac005c534
Eric Moyer <ejmoyer3@comcast.net>
2020-12-07 23:12:32 -0500

    Copied over code from family member home page, since these two pages are similar enough for it to work with a few fixes.

--- patient/patient-home.php
+++ patient/patient-home.php
@@ -21,7 +21,7 @@ echo <<<EOT
 <input type=submit value=Logout>
 </form>
 EOT;
-
+$patient = 0;
 if ($stmt = $mysqli->prepare("SELECT patientId, firstName, lastName FROM patients JOIN users ON patients.userId = users.id WHERE users.id = ?")) {
   $stmt->bind_param("i", $_SESSION['user']);
   $stmt->execute();
@@ -43,6 +43,109 @@ if ($stmt = $mysqli->prepare("SELECT patientId, firstName, lastName FROM patient
   </form>
   EOT, $patientId, $firstName, $lastName);
   }
+  $patient = $patientId;
   $stmt->close();
-}
+
+  if (isset($_POST['checkboxDate'])) {
+    echo <<<EOT
+      <table>
+      <thead>
+        <tr>
+          <th scope="col">Doctor's Name</th>
+          <th scope="col">Doctor's Appointment</th>
+          <th scope="col">Caregiver Name</th>
+          <th scope="col">Morning Medicine</th>
+          <th scope="col">Afternoon Medicine</th>
+          <th scope="col">Night Medicine</th>
+          <th scope="col">Breakfast</th>
+          <th scope="col">Lunch</th>
+          <th scope="col">Dinner</th>
+        </tr>
+      </thead>
+      <tbody>
+      EOT;
+      // query to get the doctor name
+      if ($stmt = $mysqli->prepare("SELECT firstName, lastName FROM users JOIN employees ON employees.userId = users.id JOIN rosters ON employees.employeeId = rosters.doctorId WHERE rosters.rosterDate = ?")) {
+        $stmt->bind_param("s", $_POST['checkboxDate']);
+        $stmt->execute();
+        $stmt->bind_result($docFirstName, $docLastName);
+        while ($stmt->fetch()) {
+          printf (<<<EOT
+            <tr scope="row">
+            <td>%s %s</td>
+            EOT, $docFirstName, $docLastName);
+        }
+        $stmt->close();
+        // check if an appointment exists first
+        if ($stmt = $mysqli->prepare("SELECT * FROM appointments JOIN patients ON patients.patientId = appointments.patientId JOIN users ON users.id = patients.userId WHERE appDate = ? and patients.patientId = ?;")) {
+          $stmt->bind_param("si", $_POST['checkboxDate'], $patient);
+          $stmt->execute();
+
+          $stmt->store_result();
+          // if there isn't an appointment, the checkbox will be blank. Otherwise it will be checked.
+          if ($stmt->num_rows() == 0) {
+            echo ("<td><input type=checkbox disabled></td>");
+          } else {
+            echo ("<td><input type=checkbox checked disabled></td>");
+          }
+          $stmt->close();
+          // query to get the rest of the table (caregiver and all the checkboxes)
+          if ($stmt = $mysqli->prepare("SELECT firstName, lastName, morningMed, afternoonMed, nightMed, breakfast, lunch, dinner FROM users JOIN employees ON employees.userId = users.id JOIN checkboxes ON employees.employeeId = checkboxes.caregiverId WHERE checkboxDate = ? and checkboxes.patientId = ?;")) {
+            $stmt->bind_param("si", $_POST['checkboxDate'], $patient);
+            $stmt->execute();
+            $stmt->bind_result($careFirstName, $careLastName, $morningMed, $afternoonMed, $nightMed, $breakfast, $lunch, $dinner);
+            while ($stmt->fetch()) {
+              printf (<<<EOT
+              <td>%s %s</td>
+              EOT, $careFirstName, $careLastName);
+
+              if ($morningMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($afternoonMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($nightMed == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($breakfast == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($lunch == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+
+              if ($dinner == 1) {
+                echo "<td><input type=checkbox checked disabled></td>";
+              } else {
+                echo "<td><input type=checkbox disabled></td>";
+              }
+            }
+            $stmt->close();
+
+            echo <<<EOT
+            </tr>
+            </tbody>
+            </table>
+            EOT;
+          }
+        }
+      }
+    }
+  }
+$mysqli->close();
 ?>
